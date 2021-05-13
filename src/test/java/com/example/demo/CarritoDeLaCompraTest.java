e1b67ddcd88154bc0b58c4aa73d055e30aaaf31e
David uribe <Daviduribe0007@gmail.com>
2021-05-12 22:52:09 -0500

    Add dependencies junit to pom and create the test for CarritoDeLaCompra

---
+++ src/test/java/com/example/demo/CarritoDeLaCompraTest.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class CarritoDeLaCompraTest {
+    @Test
+    public void shouldReturnTheCountOfAllItems() throws
+            Exception {
+        CarritoBuilder builder = new CarritoBuilder(30);
+        CarritoDeLaCompra carritoDeLaCompra = builder.build();
+        Assert.assertEquals(30,
+                carritoDeLaCompra.contarNumeroProductos());
+    }
+    @Test
+    public void shouldCalculateTotalPrice() throws Exception {
+        CarritoBuilder builder = new CarritoBuilder(60,5);
+        CarritoDeLaCompra carritoDeLaCompra = builder.build();
+        Assert.assertEquals(300,
+                carritoDeLaCompra.calcularPrecioTotal());
+    }
+
+}
